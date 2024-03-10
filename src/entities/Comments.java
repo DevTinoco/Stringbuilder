c80c4e90acbe5fa1c30b93cb48c4355cc30d8c26
Valdinei Tinoco <valdinei_ct@hotmail.com>
2024-03-10 20:41:24 -0300

    Constructor

--- src/entities/Comments.java
+++ src/entities/Comments.java
@@ -2,4 +2,31 @@ package entities;
 
 public class Comments {
 
+	
+		private String text;
+		
+		public Comments () {
+			
+		}
+
+		public Comments(String text) {
+			
+			this.text = text;
+		}
+
+		public String getText() {
+			return text;
+		}
+
+		public void setText(String text) {
+			this.text = text;
+		}
+		
+	
+	
+	
+	
+	
+	
+	
 }
--- src/entities/Post.java
+++ src/entities/Post.java
@@ -1,5 +1,71 @@
 package entities;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class Post {
 
+	private Date moments;
+	private String title;
+	private String content;
+	private Integer likes;
+	
+	private List<Comments> comment = new ArrayList<>();
+	
+	//CRIANDO OS CONTRUTORES, PRIMEIRO GENERATE CONSTRUCTOR USING FIELDS E APOS GENERATE:
+	public Post() {
+		
+	}
+
+	public Post(Date moments, String title, String content, Integer likes) {
+		
+		this.moments = moments;
+		this.title = title;
+		this.content = content;
+		this.likes = likes;
+	}
+
+	public Date getMoments() {
+		return moments;
+	}
+
+	public void setMoments(Date moments) {
+		this.moments = moments;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Integer getLikes() {
+		return likes;
+	}
+
+	public void setLikes(Integer likes) {
+		this.likes = likes;
+	}
+
+	public List<Comments> getComment() {
+		return comment;
+	}
+
+	public void setComment(List<Comments> comment) {
+		this.comment = comment;
+	}
+	
+	
+	
 }
